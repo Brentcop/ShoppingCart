328119871c5fe50a796965db0abeebdf4baa48e7
brentcop <coppinger.brent@student.oaklandcc.edu>
2018-05-21 18:30:41 -0400

    first commit

---
+++ capJS.js
@@ -0,0 +1,25 @@
+	var items = [
+{item : "apples", price : 1.99},
+{item : "grapes", price : 2.99},
+{item : "pizza", price : 8.99},
+{item : "noodles", price : 7.99},
+{item : "bannanas", price : 7.55}];
+var newList=[];
+var total =0;
+function addToCart(i) {
+		newList.push(items[i]);
+}
+function getTotal(){
+	//J you were righ, I swapped i w/ newList.length and it worked
+	for (var i=0;i < newList.length;i++){
+ 	total += newList[i].price; 
+
+	}
+	console.log("Your total price is: " + total);
+}
+function checkOut(){
+	for (var i = 0; i < newList.length; i++){
+		console.log(newList[i]);
+	}
+	getTotal();
+}
